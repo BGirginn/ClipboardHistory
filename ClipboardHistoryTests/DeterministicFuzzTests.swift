8329514b8a81e9829ec78c646cc18035d6aa75f3
BGirgin <girginbora30@gmail.com>
2026-08-01 15:58:15 +0300

    test: expand deterministic media fuzz corpus

--- ClipboardHistoryTests/DeterministicFuzzTests.swift
+++ ClipboardHistoryTests/DeterministicFuzzTests.swift
@@ -20,6 +20,43 @@ final class DeterministicFuzzTests: XCTestCase {
             }
         }
     }
+
+    func testExpandedMalformedMediaCorpusFailsSafely() async {
+        let signatures: [Data] = [
+            Data([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]), // PNG
+            Data([0xFF, 0xD8, 0xFF]), // JPEG
+            Data("GIF89a".utf8),
+            Data([0x49, 0x49, 0x2A, 0x00]), // TIFF
+            Data("BM".utf8),
+            Data([0x00, 0x00, 0x00, 0x18, 0x66, 0x74, 0x79, 0x70, 0x68, 0x65, 0x69, 0x63]), // HEIC
+            Data("%PDF-1.7\n".utf8),
+            Data("{\\rtf1\\ansi ".utf8)
+        ]
+        let processor = ClipboardProcessingService()
+        var generator = SplitMix64(seed: 0x4D45_4449_4143_4F52)
+
+        for caseIndex in 0..<512 {
+            let signature = signatures[caseIndex % signatures.count]
+            let suffixLength = Int(generator.next() % 1_024)
+            var bytes = signature
+            bytes.append(contentsOf: (0..<suffixLength).map { _ in
+                UInt8(truncatingIfNeeded: generator.next())
+            })
+
+            _ = ImageMetadataUtility.dimensions(for: bytes)
+            _ = await processor.process(.images(data: [bytes]), sourceBundleIdentifier: nil)
+            _ = await processor.process(.pdf(data: bytes), sourceBundleIdentifier: nil)
+            _ = await processor.process(
+                .text(
+                    value: String(decoding: bytes, as: UTF8.self),
+                    rtfData: bytes,
+                    htmlData: bytes
+                ),
+                sourceBundleIdentifier: nil
+            )
+            _ = HTMLSanitizer.sanitize(bytes)
+        }
+    }
 }
 
 private struct SplitMix64 {
